463400bbb67364de00897a4b56bca03529c2f4a8
Wahaj <Wahaj@DESKTOP-PJ4N3E4>
2024-04-08 04:17:26 -0700

    Add project files.

---
+++ Controllers/Name.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Simple_Api_Development.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Name : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("My name is Wahaj");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string statement)
+        {
+            return Ok(statement + "Ahmed");
+        }
+
+    
+        [HttpPost("Come", Name = "Come")]
+        public IActionResult Come([FromBody] string data)
+        {
+            string myTarget = "Wahaj";
+
+            int index = data.IndexOf(myTarget);
+            if (index != -1)
+            {
+                string result = data.Substring(index);
+                return Ok("My substring is: " + result);    
+            }
+            else
+            {
+                return NotFound("Keyword is not present");
+            }
+            
+        }
+        [HttpPut]
+        public IActionResult Put([FromBody] string data)
+        {
+            string myTarget = "Wahaj";
+
+            int index = data.IndexOf(myTarget);
+            if (index != -1)
+            {
+                string result = data.Replace(myTarget, myTarget.ToUpper(), StringComparison.OrdinalIgnoreCase);
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound("Keyword is not present");
+            }   
+        }
+
+
+   
+        
+        
+
+    }
+}
---
+++ Model/name.cs
@@ -0,0 +1,19 @@
+﻿namespace Simple_Api_Development.Model
+{
+    public class Name
+    {
+        private string fname {  get; set; }
+        private string lname { get; set; }
+
+        public Name() 
+        {
+        }
+
+        public Name(string fname, string lname)
+        {
+            this.fname = fname;
+            this.lname = lname;
+        }
+    }
+
+}
